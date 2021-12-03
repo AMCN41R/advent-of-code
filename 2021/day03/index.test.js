d83251babecd22d043f02e61ede9128274f48797
Alex McNair <alex.mcnair@hotmail.co.uk>
2021-12-03 12:31:01 +0000

    2021 - day03

--- 2021/day03/index.test.js
+++ 2021/day03/index.test.js
@@ -2,18 +2,18 @@ global.console = { log: jest.fn() }
 const index = require("./index");
 
 test("part one - test", () => {
-  expect(index.partOne(index.testInput)).toBe(0);
+  expect(index.partOne(index.testInput)).toBe(198);
 })
 
 test("part one", () => {
-  expect(index.partOne(index.input)).toBe(0);
+  expect(index.partOne(index.input)).toBe(3958484);
 })
 
 test("part two - test", () => {
-  expect(index.partTwo(index.testInput)).toBe(0);
+  expect(index.partTwo(index.testInput)).toBe(230);
 })
 
 test("part two", () => {
-  expect(index.partTwo(index.input)).toBe(0);
+  expect(index.partTwo(index.input)).toBe(1613181);
 })
 
--- 2021/day03/index.ts
+++ 2021/day03/index.ts
@@ -4,17 +4,98 @@ const log = console.log;
 
 export const input = utils.getLines(__dirname + "\\input.txt");
 
-export const testInput: string[] = [];
+export const testInput: string[] = [
+  '00100',
+  '11110',
+  '10110',
+  '10111',
+  '10101',
+  '01111',
+  '00111',
+  '11100',
+  '10000',
+  '11001',
+  '00010',
+  '01010',
+];
+
 
 logPartOne();
 
-export const partOne = () => {
-  return 0;
+export const partOne = (items: string[]): number => {
+
+  const ga = items.reduce((prev: number[], next: string) => {
+    if (prev.length === 0) return [...next].map(Number);
+    [...next].map(Number).forEach((x, i) => prev[i] = prev[i] + x);
+    return prev;
+  }, []).map(x => x >= (items.length / 2) ? 1 : 0);
+
+  const ea = ga.map(x => x === 1 ? 0 : 1);
+
+  const gamma = { b: ga.join(""), d: parseInt(ga.join(""), 2) };
+  const epsilon = { b: ea.join(""), d: parseInt(ea.join(""), 2) };
+
+  log("gamma:", gamma);
+  log("epsilon:", epsilon);
+
+  const result = gamma.d * epsilon.d;
+  log("result:", result);
+  log();
+
+  return result;
 }
+
+log("test");
+partOne(testInput); // expected: 198
+
+log("result");
+partOne(input); // answer: 3958484
+
 
 logPartTwo();
 
-export const partTwo = () => {
-  return 0;
+const commonBit = (most: boolean, pos: number, items: number[][]): 1 | 0 => {
+  const mostCommon = items.map(x => x[pos])
+    .reduce((prev, next) => prev + next, 0) >= (items.length / 2)
+    ? 1
+    : 0;
+
+  return most
+    ? mostCommon
+    : mostCommon === 1 ? 0 : 1
 }
 
+const filter = (items: number[][], mostCommon: boolean): { b: string, d: number } | null => {
+  let set = items.slice(0);
+  for (let i = 0; i < set[0].length; i++) {
+    set = set.filter(x => x[i] === commonBit(mostCommon, i, set));
+
+    if (set.length === 1) {
+      return { b: set[0].join(""), d: parseInt(set[0].join(""), 2) };
+    }
+  }
+
+  return null;
+}
+
+export const partTwo = (items: string[]): number => {
+  let set = items.map(x => [...x].map(Number));
+
+  const ox = filter(set, true);
+  const co = filter(set, false);
+
+  const result = (ox?.d || 0) * (co?.d || 0);
+
+  log("OX:", ox);
+  log("CO:", co);
+  log("result:", result);
+  log();
+
+  return result;
+}
+
+log("test");
+partTwo(testInput); // expected: 230
+
+log("result");
+partTwo(input); // answer: 1613181
